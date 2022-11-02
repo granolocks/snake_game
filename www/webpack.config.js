c3cec124bbdaf5f00e0f189dbe4d728a63302e5a
Gabe Koss <gabe.koss@gmail.com>
2022-11-02 08:59:08 -0400

    more webpack config

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,10 +1,16 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "public"),
-    filename: "index.js"
+    filename: "index.js",
   },
-  mode: "development"
-}
+  mode: "development",
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "./index.html", to: "./" }],
+    }),
+  ],
+};
